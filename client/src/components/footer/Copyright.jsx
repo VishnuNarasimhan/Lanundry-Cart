2846fe7d12be40963eae26327b4389e4b0db913a
VishnuNarasimhan <vishnunarasim20@gmail.com>
2022-06-04 13:34:44 +0530

    Final

---
+++ client/src/components/footer/Copyright.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Copyright = () => {
+  return (
+    <div className='copyright-wrapper'>
+      <div className='copyright-info'>2022 © Laundry</div>
+    </div>
+  );
+};
+
+export default Copyright;
